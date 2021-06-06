d3f69a76e6da64fb9bbc626fa9636b076b91cb61
Guido Salimbeni <guido.salimbeni@gmail.com>
2021-06-06 19:08:58 +0100

    get for test

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 # from nlp.similarity import Detect
 from nlp.gpt_generation import TextGenerationGpt2
 
+
 app = Flask(__name__)
 api = Api(app)
 
+class Hello(Resource):
+    def get(self):
+        
+        retJson = {
+            "status":200,
+            "ratio": "Hello World",
+            "msg":"text generated successfully"
+        }
+
+        return jsonify(retJson)
 
 # api.add_resource(Detect, '/detect')
+api.add_resource(Hello, '/')
 api.add_resource(TextGenerationGpt2, '/generate')
 
 app.run(port=5000)
