a8cbf41ca6d22e9dc76b614d58e3ea943b30f216
Guidosalimbeni <guido.salimbeni@gmail.com>
2021-06-06 16:35:14 +0100

    added logic to generated text

--- nlp/gpt_generation.py
+++ nlp/gpt_generation.py
@@ -1,6 +1,8 @@
 from transformers import pipeline, set_seed
 from flask import Flask, jsonify, request
 from flask_restful import Resource
+import random
+
 
 generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
@@ -10,9 +12,20 @@ class TextGenerationGpt2(Resource):
         #Step 1 get the posted data
         postedData = request.get_json()
         text = postedData["user_text"]
-        
-        chat_list = generator(str(text), max_length=30, num_return_sequences=2) 
-        chat = chat_list[0]
+        number = 3
+        chat_list = generator(str(text), max_length=30, num_return_sequences=number) 
+        chat = chat_list[random.randint(0,number)]["generated_text"]
+        chat = chat.replace(text, "")
+        chats = chat.split(".")
+        chats_2 = []
+        for sent in chats:
+            if len(sent) > 0:
+                chats_2.append(sent)
+        chat = chats_2[0]
+        chat = chat.replace(".", "")
+        chat = chat + "."
+        if chat[0] == " ":
+            chat = chat[1:]
         retJson = {
             "status":200,
             "ratio": chat,
