2c6e4248a9b09bcfc5fcd2db92db51c2cc25a540
Guido Salimbeni <guido.salimbeni@gmail.com>
2021-06-06 14:06:38 +0100

    added generate resource from gpt2

--- nlp/gpt_generation.py
+++ nlp/gpt_generation.py
@@ -1,7 +1,22 @@
 from transformers import pipeline, set_seed
+from flask import Flask, jsonify, request
+from flask_restful import Resource
+
 generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
-a = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
 
-print(a)
+class TextGenerationGpt2(Resource):
+    def post(self):
+        #Step 1 get the posted data
+        postedData = request.get_json()
+        text = postedData["user_text"]
+        
+        chat_list = generator(str(text), max_length=30, num_return_sequences=2) 
+        chat = chat_list[0]
+        retJson = {
+            "status":200,
+            "ratio": chat,
+            "msg":"text generated successfully"
+        }
 
+        return jsonify(retJson)
