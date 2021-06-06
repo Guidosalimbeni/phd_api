fb153c843791b040f0356a6c68d26d69521143b4
Guido Salimbeni <guido.salimbeni@gmail.com>
2021-06-06 18:03:38 +0100

    reduced size for deployement

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from nlp.similarity import Detect
+# from nlp.similarity import Detect
 from nlp.gpt_generation import TextGenerationGpt2
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(Detect, '/detect')
+# api.add_resource(Detect, '/detect')
 api.add_resource(TextGenerationGpt2, '/generate')
 
 app.run(port=5000)
--- nlp/similarity.py
+++ nlp/similarity.py
@@ -1,27 +1,27 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource
+# from flask import Flask, jsonify, request
+# from flask_restful import Resource
 
-class Detect(Resource):
-    def post(self):
-        #Step 1 get the posted data
-        postedData = request.get_json()
-        text1 = postedData["text1"]
-        text2 = postedData["text2"]
-
-
-        #Calculate edit distance between text1, text2
-        import spacy
-        nlp = spacy.load('en_core_web_sm')
-        text1 = nlp(text1)
-        text2 = nlp(text2)
-
-        ratio = text1.similarity(text2)
-
-        retJson = {
-            "status":200,
-            "ratio": ratio,
-            "msg":"Similarity score calculated successfully"
-        }
+# class Detect(Resource):
+#     def post(self):
+#         #Step 1 get the posted data
+#         postedData = request.get_json()
+#         text1 = postedData["text1"]
+#         text2 = postedData["text2"]
 
 
-        return jsonify(retJson)
+#         #Calculate edit distance between text1, text2
+#         import spacy
+#         nlp = spacy.load('en_core_web_sm')
+#         text1 = nlp(text1)
+#         text2 = nlp(text2)
+
+#         ratio = text1.similarity(text2)
+
+#         retJson = {
+#             "status":200,
+#             "ratio": ratio,
+#             "msg":"Similarity score calculated successfully"
+#         }
+
+
+#         return jsonify(retJson)
